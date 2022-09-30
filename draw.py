7aeac839810729d404516acc99069ddf40e8ec25
guoyork <49684611+guoyork@users.noreply.github.com>
2022-09-30 10:12:11 +0800

    Add files via upload

--- draw.py
+++ draw.py
@@ -1,18 +1,40 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-a = np.loadtxt("1.txt")
+a = np.loadtxt("offline_3.txt")
+b = np.loadtxt("online_3.txt")
+c = np.loadtxt("ground_truth_3.txt")
 
-b = [abs(a[i+2]-a[0]) for i in range(len(a)-2)]
-x = np.asarray(range(len(b)))/10
+n = len(c)
+x = np.asarray(range(n))
+'''
+plt.xlabel('m/n')
+plt.ylabel('average bias')
 
+temp1 = a[:, 0]
+temp2 = b[:, 0]
+temp1 = np.abs(temp1 - c)
+temp2 = np.abs(temp2 - c)
+for i in range(n):
+    temp1[i] /= 5 * (i + 1)
+    temp2[i] /= 5 * (i + 1)
+plt.plot(x, temp1[0:n], color='green', label='abtests')
+plt.plot(x, temp2[0:n], color='red', label='optimal experiment')
 
-plt.xlabel('epsilon')
-plt.ylabel('bias')
-
-plt.plot(x, b, marker='s', color='green', label='our algorithm')
-plt.axhline(y=abs(a[0]-a[1]), color='red', linestyle="--", label='A/B testing')
 plt.legend(loc='upper right')
+
+plt.grid()
+plt.show()
+'''
+plt.xlabel('m/n')
+plt.ylabel('variance')
+
+temp1 = a[:, 1]
+temp2 = b[:, 1]
+plt.plot(x, temp1[0:n], color='green', label='offline')
+plt.plot(x, temp2[0:n], color='red', label='online')
+
+plt.legend(loc='lower right')
 
 plt.grid()
 plt.show()
